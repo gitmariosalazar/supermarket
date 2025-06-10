c457ff8184c5a98ec2b306854e9b4e7c771667d3
Mario Salazar <mariosalazar.ms.10@gmail.com>
2025-06-09 23:37:51 -0500

    feat(customers): add new method - Response To Request

--- supermarket/src/modules/customers/infrastructure/adapters/customer.adapter.ts
+++ supermarket/src/modules/customers/infrastructure/adapters/customer.adapter.ts
@@ -1,5 +1,6 @@
 import { PersonModel } from '../../../../shared/modules/person/domain/schemas/model/person.model';
 import { PersonAdapter } from '../../../../shared/modules/person/infrastructure/adapters/person.adapter';
+import { CustomerRequest } from '../../domain/schemas/dto/request/customer.request';
 import { CustomerResponse } from '../../domain/schemas/dto/response/customer.response';
 import { CustomerModel } from '../../domain/schemas/model/customer.model';
 
@@ -22,5 +23,21 @@ export class CustomerAdapter {
       )
     };
     return customerResponse;
+  }
+
+  static customerResponseToCustomerRequest(
+    customerResponse: CustomerResponse
+  ): CustomerRequest {
+    const customerRequest: CustomerRequest = new CustomerRequest(
+      customerResponse.idCustomer,
+      customerResponse.birthDate,
+      customerResponse.person.cardId,
+      customerResponse.person.firstName,
+      customerResponse.person.lastName,
+      customerResponse.person.email,
+      customerResponse.person.address,
+      customerResponse.person.phone
+    );
+    return customerRequest;
   }
 }
