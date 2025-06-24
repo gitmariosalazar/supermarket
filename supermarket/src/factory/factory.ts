4377b2e22661ad92a53abf9ab0ba2ef62fbcc229
Mario Salazar <mariosalazar.ms.10@gmail.com>
2025-06-23 20:47:10 -0500

    feat(factory): add factory class

--- supermarket/src/factory/factory.ts
+++ supermarket/src/factory/factory.ts
@@ -1,6 +1,12 @@
 import { DatabaseMockup } from '../database/mockup/database';
+import { DatabaseServiceMySQL } from '../database/mysql/service/mysql.service';
+import { DatabaseServicePostgreSQL } from '../database/postgres/service/postgresql.service';
+import { DatabaseServiceSQLServer } from '../database/sqlserver/service/sqlserver.service';
+import { CartItemService } from '../modules/carts/application/services/cart-item.use-case.service';
 import { CartService } from '../modules/carts/application/services/cart.use-case.service';
+import { CartItemController } from '../modules/carts/infrastructure/controller/cart-item.controller';
 import { CartController } from '../modules/carts/infrastructure/controller/cart.controller';
+import { CartItemRepositoryMockupImplementation } from '../modules/carts/infrastructure/repositories/mockup/mockup.cart-item.persistence';
 import { CartRepositoryMockupImplementation } from '../modules/carts/infrastructure/repositories/mockup/mockup.cart.persistence';
 import { CustomerService } from '../modules/customers/application/services/customer.use-case.service';
 import { CustomerController } from '../modules/customers/infrastructure/controller/customer.controller';
@@ -11,6 +17,9 @@ import { ProductCategoryController } from '../modules/products/infrastructure/co
 import { ProductController } from '../modules/products/infrastructure/controller/product.controller';
 import { ProductCategoryMockupImplementation } from '../modules/products/infrastructure/repositories/mockup/mockup.product-category.persistence';
 import { ProductRepositoryMockupImplementation } from '../modules/products/infrastructure/repositories/mockup/mockup.product.persistence';
+import { ProductRepositoryMySQLImplementation } from '../modules/products/infrastructure/repositories/mysql/persistence/mysql.product.persistence';
+import { ProductRepositoryPostgreSQLImplementation } from '../modules/products/infrastructure/repositories/postgres/persistence/postgres.product.persistence';
+import { ProductRepositorySQLImplementation } from '../modules/products/infrastructure/repositories/sqlserver/persistence/sqlserver.product.persistence';
 import { InvoiceService } from '../modules/sales/application/services/invoice.use-case.service';
 import { InvoiceController } from '../modules/sales/infrastructure/controller/invoice.controller';
 import { InvoiceRepositoryMockupImplementation } from '../modules/sales/infrastructure/repositories/mockup/mockup.invoice.persistence';
@@ -21,19 +30,30 @@ import { SellerRepositoryMockupImplementation } from '../modules/sellers/infrast
 export class AppFactory {
   private databaseMockup: DatabaseMockup = new DatabaseMockup();
 
+  //private mySQLService: DatabaseServiceMySQL = new DatabaseServiceMySQL();
+  //private postgreSQLService: DatabaseServicePostgreSQL = new DatabaseServicePostgreSQL();
+
+  //private sqlService: DatabaseServiceSQLServer = new DatabaseServiceSQLServer();
+
+  //Implementation
   private cartRepo: CartRepositoryMockupImplementation =
     new CartRepositoryMockupImplementation(this.databaseMockup);
   private customerRepo: CustomerRepositoryMockupImplementation =
     new CustomerRepositoryMockupImplementation(this.databaseMockup);
+
   private productRepo: ProductRepositoryMockupImplementation =
     new ProductRepositoryMockupImplementation(this.databaseMockup);
+
   private sellerRepo: SellerRepositoryMockupImplementation =
     new SellerRepositoryMockupImplementation(this.databaseMockup);
   private invoiceRepo: InvoiceRepositoryMockupImplementation =
     new InvoiceRepositoryMockupImplementation(this.databaseMockup);
-  private readonly productsCategoryRepo: ProductCategoryMockupImplementation =
+  private productsCategoryRepo: ProductCategoryMockupImplementation =
     new ProductCategoryMockupImplementation(this.databaseMockup);
+  private cartItemRepositoryImplementation: CartItemRepositoryMockupImplementation =
+    new CartItemRepositoryMockupImplementation(this.databaseMockup);
 
+  //Services
   private cartService: CartService = new CartService(this.cartRepo);
   private customerService: CustomerService = new CustomerService(
     this.customerRepo
@@ -43,7 +63,11 @@ export class AppFactory {
   private invoiceService: InvoiceService = new InvoiceService(this.invoiceRepo);
   private productCategoryService: ProductCategoryService =
     new ProductCategoryService(this.productsCategoryRepo);
+  private cartItemService: CartItemService = new CartItemService(
+    this.cartItemRepositoryImplementation
+  );
 
+  //Controllers
   public cartController: CartController = new CartController(this.cartService);
   public customerController: CustomerController = new CustomerController(
     this.customerService
@@ -60,6 +84,10 @@ export class AppFactory {
 
   public productCategoryController: ProductCategoryController =
     new ProductCategoryController(this.productCategoryService);
+
+  public cartItemController: CartItemController = new CartItemController(
+    this.cartItemService
+  );
 
   public getDatabase(): DatabaseMockup {
     return this.databaseMockup;
